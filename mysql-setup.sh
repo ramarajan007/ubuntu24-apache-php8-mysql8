4da6a8b71c36790c15ce3ca925549c7675cf9c71
ramarajan007 <ramarajan581995@gmail.com>
2024-08-30 15:21:55 +0530

    Update mysql-setup.sh

--- mysql-setup.sh
+++ mysql-setup.sh
@@ -6,5 +6,7 @@ MYSQL_ROOT_PASSWORD="Scremr@2k23"
 # Commands to automate MySQL login and set root password
 mysql <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
+CREATE USER 'root'@'%' IDENTIFIED BY 'Scremr@2k23';
+GRANT ALL PRIVILEGES ON *.* to 'root'@'%';
 FLUSH PRIVILEGES;
 EOF
